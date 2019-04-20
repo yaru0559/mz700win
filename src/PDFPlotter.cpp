6d66b41b229365d1f93cf2e401c726dcb3d6c4a1
AOKI Yasuharu <yaru@toritori.org>
2019-04-21 07:38:43 +0900

    PDF生成場所をexeのあるフォルダに固定 線幅、点線ピッチを調整

--- src/PDFPlotter.cpp
+++ src/PDFPlotter.cpp
@@ -5,6 +5,9 @@
 #include <sstream>
 #include <iomanip>
 
+#define NOMINMAX
+#include <Windows.h>
+
 using namespace TPDFL;
 
 static const char* PENCOLOR[] = {
@@ -33,8 +36,20 @@ void PDFPlotter::NewFile()
 {
 	time_t t = time(NULL);
 	const tm* lt = localtime(&t);
-	char buf[20];
-	sprintf_s(buf, "./%02d%02d%02d%02d%02d%02d.pdf", lt->tm_year % 100, lt->tm_mon + 1, lt->tm_mday, lt->tm_hour, lt->tm_min, lt->tm_sec);
+	char buf[_MAX_PATH];
+	char drive[_MAX_DRIVE];
+	char folder[_MAX_DIR];
+	char file[_MAX_FNAME];
+	char extent[_MAX_EXT];
+	::GetModuleFileName(NULL, buf, _MAX_PATH);
+	if (0 == _splitpath_s(buf, drive, folder, file, extent)) {
+		sprintf_s(buf, "%s%s%02d%02d%02d%02d%02d%02d.pdf", drive, folder, 
+			lt->tm_year % 100, lt->tm_mon + 1, lt->tm_mday, lt->tm_hour, lt->tm_min, lt->tm_sec);
+	}
+	else {
+		sprintf_s(buf, "./%02d%02d%02d%02d%02d%02d.pdf", 
+			lt->tm_year % 100, lt->tm_mon + 1, lt->tm_mday, lt->tm_hour, lt->tm_min, lt->tm_sec);
+	}
 
 	FILE* fp(NULL);
 	fopen_s(&fp, buf, "wb");
@@ -68,9 +83,9 @@ void PDFPlotter::NewPage()
 void PDFPlotter::SetLineGState()
 {
 	std::ostringstream oss;
-	oss << "1 w 1 J 1 j" << std::endl;
+	oss << "0.75 w 1 J 1 j" << std::endl;
 	if (0 < m_lineStyle) {
-		oss << "[" << m_lineStyle * 2 << "] 0 d ";
+		oss << "[" << (double)m_lineStyle * 1.5 << "] 0 d ";
 	}
 	m_currentContents->AddStream(oss.str().c_str());
 	m_currentContents->AddStream(PENCOLOR[m_penColor]);
